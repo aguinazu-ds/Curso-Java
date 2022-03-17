063348eea416e099f2256448ef27f8b1ce30d0c2
aguinazu-ds <aeguinazu@gmail.com>
2022-03-17 16:10:24 -0300

    clase 17/03

---
+++ intro-webdev/menu.js
@@ -0,0 +1,29 @@
+var a = 0;
+var menu = "El cliente eligio: ";
+while (a != 'menu' && parseInt(a) != 6){
+    while (a<6){
+        a = prompt(
+            "----- Menu -----\n1.- Sopa\n2.- Ensalada\n3.- Carne\n4.- Postre\n5.- Bebida\n6.- Salir\n---------------\nIngrese menu o digite 6 para salir"
+        );
+        console.log(a)
+        switch (a) {
+            case "1":
+                menu = menu + "Sopa ";
+                break;
+            case "2":
+                menu = menu + "Ensalada ";
+                break;
+            case "3":
+                menu = menu + "Carne ";
+                break;
+            case "4":
+                menu = menu + "Postre ";
+                break;
+            case "5":
+                menu = menu + "Bebida ";
+                break;
+        }
+        console.log(menu)
+    }
+}
+document.write(menu)
--- intro-webdev/test.js
+++ intro-webdev/test.js
@@ -1,2 +1,15 @@
-var x = 1000;
-console.log(x);
+var precioCalzado = parseInt(prompt("Ingrese el precio del calzado"));
+
+if (precioCalzado > 10000) {
+    var descuento = precioCalzado * 0.30;
+}
+else if (precioCalzado > 5000 && precioCalzado <= 10000) {
+    descuento = precioCalzado * 0.10;
+}
+else {
+    descuento = 0;
+}
+
+var totalPagar = precioCalzado - descuento;
+
+document.write("El total a pagar es: ", totalPagar);
