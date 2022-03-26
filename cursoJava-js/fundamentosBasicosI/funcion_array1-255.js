a25053ad8846b040f4c73d77da52847b3b83b083
aguinazu-ds <aeguinazu@gmail.com>
2022-03-26 19:19:42 -0300

    suma 1000 pares

--- cursoJava-js/fundamentosBasicosI/funcion_array1-255.js
+++ cursoJava-js/fundamentosBasicosI/funcion_array1-255.js
@@ -1,9 +1,9 @@
 const array1_255 = function () {
-    let empty_array = []
+    let empty_array = [];
 
     for (i=1; i <= 255; i++) {
-        empty_array.push(i)
+        empty_array.push(i);
     }
 
-    return empty_array
+    return empty_array;
 }
