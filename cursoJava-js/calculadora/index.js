ea41103750127ff0a9bc2431bdc17a602fe1e138
aguinazu-ds <aeguinazu@gmail.com>
2022-03-26 18:12:00 -0300

    26/3

---
+++ cursoJava-js/calculadora/index.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    let resultado = document.getElementById("resultado");
+
+    document.getElementById("btnsumar").addEventListener("click", function () {
+        
+    }
+    
+    )
+}
