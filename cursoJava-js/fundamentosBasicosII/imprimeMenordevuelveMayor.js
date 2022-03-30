ddac7ce0f8a065c9c1b3e9145a0b8e3f6a438804
aguinazu-ds <aeguinazu@gmail.com>
2022-03-30 10:44:17 -0300

    Imprime menor, devuelve mayor

--- cursoJava-js/fundamentosBasicosII/imprimeMenordevuelveMayor.js
+++ cursoJava-js/fundamentosBasicosII/imprimeMenordevuelveMayor.js
@@ -1,1 +1,16 @@
-const menor_mayor = function()
+const menor_mayor = function(array) {
+    max = array[0];
+    min = array[0];
+
+    array.forEach(element => {
+        if (max < element) {
+            max = element;
+        };
+        if (min > element) {
+            min = element;
+        };
+    });
+
+    console.log(min);
+    return max;
+}
